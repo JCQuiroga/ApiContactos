10e5d5c248c277060d504394f8dc40d4f29ae085
JCQuiroga <josecquiroga@gmail.com>
2016-01-26 09:29:59 +0100

    Primera subida

---
+++ ApiContactos/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.DynamicData;
+using ApiContactos.Adapters;
+using ApiContactos.Models;
+using ContactosModel.Model;
+using RepositorioAdapter.Repositorio;
+
+namespace ApiContactos.Repositorios
+{
+    public class UsuarioRepositorio : BaseRepositorioEntity<Usuario, UsuarioModel, UsuarioAdapter>
+    {
+        public UsuarioRepositorio(DbContext context) : base(context)
+        { //Necesita implementar el constructor para que le pase el context. No hace falta implementar nada aqui dentro.
+        }
+
+        public UsuarioModel Validar(string login, string password)
+        {
+            var data = Get(o => o.login == login && o.password == password);
+            if (data.Any()) return data.First();
+            return null;
+        }
+
+        public bool isUnico(string login) //para saber si ya existe un usuario con ese login (antes de crearlo).
+        {
+            var data = Get(o => o.login == login);
+            return !data.Any();
+        }
+
+        public override UsuarioModel Add(UsuarioModel model)
+        {
+            if (isUnico(model.login)) return base.Add(model);
+            return null;
+        }
+    }
+}
---
+++ RepositorioAdapter/Adapter/BaseAdapter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RepositorioAdapter.Adapter
+{
+    public abstract class BaseAdapter<TEntity, TModel> : IAdapter<TEntity, TModel>
+    {
+        public abstract TEntity FromViewModel(TModel model);
+
+        public abstract TModel FromModel(TEntity model);
+
+        public ICollection<TEntity> FromViewModel(ICollection<TModel> model)
+        {
+            return model.Select(FromViewModel).ToList();
+        }
+
+        public ICollection<TModel> FromModel(ICollection<TEntity> model)
+        {
+            return model.Select(FromModel).ToList();
+            /*Es equivalente a hacer:
+            var retorno = new List<TModel>();
+            foreach(var entity in model)
+            {
+            retorno.Add(FromModel(entity));
+            }
+            return retorno;
+            */
+
+        }
+    }
+}
